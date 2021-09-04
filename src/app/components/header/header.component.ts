be2aff3bff209be5085168c9a581f479787d0266
dtoro97 <torod76@icloud.com>
2021-09-04 11:58:37 +0200

    [fix] close navbar when clicked outside of it

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -10,24 +10,37 @@ import { Subscription } from 'rxjs';
 export class HeaderComponent implements OnDestroy, OnInit {
     public isNavOpened = false;
     public subscription = new Subscription();
+    private wasClickInside = false;
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
-
-  public toggleNav(): void {
-      this.isNavOpened = !this.isNavOpened;
-  }
-
-  public ngOnInit(): void {
-    this.subscription = this.breakpointObserver.observe([
-        Breakpoints.HandsetLandscape,
-        Breakpoints.HandsetPortrait,
-        Breakpoints.XSmall
-    ]).subscribe((state: BreakpointState) => {
-        if (state.breakpoints[Breakpoints.XSmall]) {
+    @HostListener('click')
+    clickInside() {
+        this.wasClickInside = true;
+    }
+    @HostListener('document:click')
+    clickout() {
+        if (!this.wasClickInside) {
             this.isNavOpened = false;
         }
-    });
-  }
+        this.wasClickInside = false;
+    }
+
+    constructor(private breakpointObserver: BreakpointObserver) {}
+
+    public toggleNav(): void {
+        this.isNavOpened = !this.isNavOpened;
+    }
+
+    public ngOnInit(): void {
+        this.subscription = this.breakpointObserver.observe([
+            Breakpoints.HandsetLandscape,
+            Breakpoints.HandsetPortrait,
+            Breakpoints.XSmall
+        ]).subscribe((state: BreakpointState) => {
+            if (state.breakpoints[Breakpoints.XSmall]) {
+                this.isNavOpened = false;
+            }
+        });
+    }
 
   public ngOnDestroy(): void {
       this.subscription.unsubscribe();
